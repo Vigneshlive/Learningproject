f1378213b4a3b0ff12048d8c5fc8c10f8ee6683d
Soundararajan <vignesh.soundararajan@sap.com>
2023-03-28 19:40:14 +0530

    learning the basics

--- add.py
+++ add.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-print ('Learning the github for new')
-=======
-print ('Learning the github and git')
->>>>>>> 46994654a5f5587a2d82e8f4a995fc96d2c0c714
+print ('Learning the github for first time')
 print ('Adding the 2 numbers')
 Print ('New code')
